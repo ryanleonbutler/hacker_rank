d0f9b3b8dafc3cdc68a7b90be58d21101a35f2bb
Ryan <butryan@amazon.com>
2020-03-12 20:15:07 +0200

    Got to 4 stars yay

--- challenges/lists.py
+++ challenges/lists.py
@@ -1,5 +1,12 @@
-def split_input(arguments):
-    l = arguments.split()
-
 if __name__ == '__main__':
     N = int(input())
+    my_list = []
+    for i in range(N):
+        string = input().split()
+        cmd = string[0]
+        args = string[1:]
+        if cmd != "print":
+            cmd += "(" + ",".join(args) + ")"
+            eval("my_list." + cmd)
+        else:
+            print(my_list)
--- challenges/lists_test.py
+++
@@ -1,8 +0,0 @@
-import lists
-
-MY_LIST = []
-
-
-def test_lists():
-    assert list_insert(MY_LIST, 0, 5) == [5]
-
