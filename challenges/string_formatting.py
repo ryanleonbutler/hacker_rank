5b9d0be03a8618fbaeeaa4bfc69f20c0c5d2eefe
Ryan <butryan@amazon.com>
2020-03-19 20:47:44 +0200

    Completed Python Challenge.

---
+++ challenges/string_formatting.py
@@ -0,0 +1,10 @@
+def print_formatted(number):
+    # your code goes here
+    width = len(format(number, 'b'))
+    for i in range(1, number + 1):
+        print("{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=width))
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
